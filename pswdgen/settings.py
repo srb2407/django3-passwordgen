b7963ebd211e37a15cc7638f6440577b6dd8f4c1
Saurabh Shakya <shakya.saurabh45@gmail.com>
2020-09-29 15:03:50 +0530

    Update settings.py

--- pswdgen/settings.py
+++ pswdgen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w3&6mt*yi2mtxr0pg-i(s-0ga+&i*nxqhzyqemd*_u4&=ap*9&'
+SECRET_KEY = 'herdfgw3&6mt*yi2mtxr0pgergerg-i(s-0ga+&i*nxergergergqhzyqemgggd*_u4&=ap*9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
